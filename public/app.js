c628b0c963848c6cf0bd998d7f81a2b37ce44756
Anirudh <ani.for.prez@gmail.com>
2014-09-14 21:45:31 +0530

    incuded all scripts in index.html

--- public/app.js
+++ public/app.js
@@ -1,13 +1,18 @@
 var BingeApp = angular.module('BingeApp', ['ngRoute', 'ngMessages', 'ngResource']);
 
-MyApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+BingeApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
 
 	$locationProvider.html5Mode(true);
 
 	$routeProvider
 		.when('/', {
-			templateUrl: '',
-			controller : ''
+			templateUrl: 'views/home.html'
 		})
-
+		.when('/show/:id', {
+			templateUrl: 'views/detail.html',
+			controller : 'DetailCtrl'
+		})
+		.otherwise({
+			redirectTo : '/'
+		});
 }]);
