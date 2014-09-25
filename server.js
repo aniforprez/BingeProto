3169da64913817669f1986ded9a1fb011f33a4bb
Anirudh <ani.for.prez@gmail.com>
2014-09-25 22:37:33 +0530

    replaced app routing with express.Router
    alsoe added fontawesome and request

--- server.js
+++ server.js
@@ -10,23 +10,25 @@ app.set('port', process.env.port || 3000);
 app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(app.get('port'), function() {
-	console.log("Express server on port " + app.get('port'));
+router.use(function(err, req, res, next) {
+	console.error(err.stack);
+	res.send(500, { message: err.message });
 });
 
-app.get('/api/show', function(req, res, next) {
+router.get('/api/show', function(req, res, next) {
 	res.send([{ result: req.query.searchString }]);
 });
 
-app.get('/api/show/:id', function(req, res, next) {
+router.get('/api/show/:id', function(req, res, next) {
 	res.send({ message: "Here's data for show " + req.params.id });
 });
 
-app.get('*', function(req, res) {
+router.get('*', function(req, res) {
 	res.redirect('/#' + req.originalUrl);
 });
 
-app.use(function(err, req, res, next) {
-	console.error(err.stack);
-	res.send(500, { message: err.message });
+app.use('/', router);
+
+app.listen(app.get('port'), function() {
+	console.log("Express server on port " + app.get('port'));
 });
