edf5c24e9def69ad0883067ec2889f3ab373e68f
Anirudh <ani.for.prez@gmail.com>
2014-09-26 15:10:05 +0530

    search now fetches results from TVDB

--- public/controllers/search.js
+++ public/controllers/search.js
@@ -1,6 +1,5 @@
 angular.module('BingeApp')
 	.controller('SearchCtrl', ['$scope', '$location', 'Show', function($scope, $location, Show){
 		var searchObj = $location.search();
-		console.log(searchObj);
-		$scope.result = Show.query({ searchString: searchObj.name });
+		$scope.searchResults = Show.query({ searchString: searchObj.name });
 	}]);
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
+var express  = require('express');
+var path     = require('path');
+var logger   = require('morgan');
 var mongoose = require('mongoose');
+var request  = require('request');
+var xml2js = require('xml2js');
 
-var app = express();
+var app    = express();
 var router = express.Router();
 
 app.set('port', process.env.port || 3000);
@@ -16,7 +18,26 @@ router.use(function(err, req, res, next) {
 });
 
 router.get('/api/show', function(req, res, next) {
-	res.send([{ result: req.query.searchString }]);
+	var search = req.query.searchString
+					.toLowerCase()
+					.replace(/ /g, '_')
+					.replace(/[^\w-]+/g, '');
+	var xmlParser = xml2js.Parser({
+		explicitArray: false,
+		normalizeTags: true
+	});
+
+	request.get('http://thetvdb.com/api/GetSeries.php?seriesname=' + search, function(error, response, body) {
+		if(error)
+			return next(error);
+		xmlParser.parseString(body, function(error, result) {
+			if(!result.data.series) {
+				return res.send({ message: "No search results" });
+			}
+			res.send(result.data.series);
+		});
+	});
+	// res.send([{ result: req.query.searchString }]);
 });
 
 router.get('/api/show/:id', function(req, res, next) {
