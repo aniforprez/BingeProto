16726299820bb9cbe231d8cb6cbc219489a1a499
Anirudh <ani.for.prez@gmail.com>
2014-09-29 18:10:16 +0530

    tvdb fetching waterfall moved to show.getShowData

--- server.js
+++ server.js
@@ -48,6 +48,8 @@ var showSchema = new mongoose.Schema({
 	status        : String,
 	banner        : String,
 	poster        : String,
+	noOfSeasons   : Number,
+	noOfEpisodes  : Number,
 	actors: [{
 		name     : String,
 		role     : String,
@@ -69,6 +71,122 @@ var showSchema = new mongoose.Schema({
 		}]
 	}]
 });
+showSchema.methods.getShowData = function(seriesID, rootCallback) {
+	var apiKey = 'F917081C46B60FCD';
+	var show   = this;
+
+	async.waterfall([
+		function(callback) {
+			console.log("Getting showdata");
+			request.get('http://thetvdb.com/api/' + apiKey + '/series/' + seriesID + '/all/en.xml', function(error, response, body) {
+				if(error)
+					next(error);
+				xmlParser.parseString(body, function(err, result) {
+					callback(err, result.data);
+				});
+			});
+		},
+		function(data, callback) {
+			var series = data.series;
+			var episodes = data.episode;
+
+			show._id          = series.id;
+			show.name         = series.seriesname;
+			show.airsDayOfWeek= series.airs_dayofweek;
+			show.airsTime     = series.airs_time;
+			show.contentRating= series.contentrating;
+			show.firstAired   = series.firstaired;
+			show.genre        = series.genre.split('|').filter(Boolean);
+			show.imdb_id      = series.imdb_id;
+			show.language     = series.language;
+			show.network      = series.network;
+			show.overview     = series.overview;
+			show.runtime      = series.runtime;
+			show.status       = series.status;
+			show.banner       = series.banner;
+			show.poster       = series.poster;
+			show.actors       = [];
+			show.seasons      = [];
+			_.each(episodes, function(episode) {
+				var episodeObj = {
+					episodeID    : episode.id,
+					episodeNumber: episode.episodenumber,
+					name         : episode.episodename,
+					director     : episode.director.split('|').filter(Boolean),
+					firstAired   : episode.firstaired,
+					guestStars   : episode.gueststars.split('|').filter(Boolean),
+					imdb_id      : episode.imdb_id,
+					overview     : episode.overview
+				};
+
+				var seasonIndex = _.findIndex(show.seasons, function(season) {
+					return season.seasonNumber == episode.seasonnumber;
+				});
+				if(seasonIndex > -1) {
+					show.seasons[seasonIndex].episodes.push(episodeObj);
+					if(episode.seasonnumber > 0) {
+						if(show.noOfEpisodes)
+							show.noOfEpisodes++;
+						else
+							show.noOfEpisodes = 1;
+					}
+				}
+				else {
+					show.seasons.push({
+						seasonNumber: episode.seasonnumber,
+						seasonID    : episode.seasonid,
+						episodes    : [episodeObj]
+					});
+					if(episode.seasonnumber > 0) {
+						if(show.noOfSeasons)
+							show.noOfSeasons++;
+						else
+							show.noOfSeasons = 1;
+
+						if(show.noOfEpisodes)
+							show.noOfEpisodes++;
+						else
+							show.noOfEpisodes = 1;
+					}
+				}
+			});
+
+			callback(null);
+		},
+		function(callback) {
+			request.get('http://thetvdb.com/api/' + apiKey + '/series/' + seriesID + '/actors.xml', function(error, response, body) {
+				if(error)
+					next(error);
+				xmlParser.parseString(body, function(err, result) {
+					_.each(result.actors.actor, function(actor) {
+						show.actors.push({
+							name     : actor.name,
+							role     : actor.role,
+							image    : actor.image,
+							sortOrder: actor.sortorder
+						});
+					});
+
+					callback(err);
+				});
+			});
+		},
+		function(callback) {
+			show.save(function(err) {
+				if(err)
+					return console.error(err);
+				callback(null, "Saved successfully");
+			});
+		}
+	], function(err, message) {
+		if(err)
+			return console.error(err);
+		console.log(message);
+		if (typeof rootCallback === "function") {
+			rootCallback();
+		}
+	});
+};
 
 var Show = mongoose.model('Show', showSchema);
 
@@ -102,122 +220,25 @@ router.get('/api/search', function(req, res, next) {
 
 // Show Details route
 router.get('/api/show/:id', function(req, res, next) {
-	var apiKey = 'F917081C46B60FCD';
 	var seriesID = req.params.id;
 
-	var endShowQuery = function(error, show) {
-		if(error)
-			return next(error);
-		res.send(show);
-	};
-
-	async.waterfall([
-		function(callback) {
-			Show.findById(seriesID, function(err, show) {
-				if(err)
-					return next(err);
-				if(show)
-					endShowQuery(null, show);
-				if(!show)
-					callback(null, "Show not in DB");
-			});
-		},
-		function(message, callback) {
-			console.log(message);
-			request.get('http://thetvdb.com/api/' + apiKey + '/series/' + seriesID + '/all/en.xml', function(error, response, body) {
-				if(error)
-					next(error);
-				xmlParser.parseString(body, function(err, result) {
-					callback(err, result.data);
+	Show.findById(seriesID, function(err, show) {
+		if(err)
+			return next(err);
+		if(show)
+			res.send(show);
+		if(!show) {
+			var tempShow = new Show({});
+			tempShow.getShowData(seriesID, function() {
+				Show.findById(seriesID, function(err, show) {
+					console.log("Finished fetching");
+					if(err)
+						return next(err);
+					res.send(show);
 				});
-			});
-		},
-		function(data, callback) {
-			var series = data.series;
-			var episodes = data.episode;
-
-			var show = new Show({
-				_id          : series.id,
-				name         : series.seriesname,
-				airsDayOfWeek: series.airs_dayofweek,
-				airsTime     : series.airs_time,
-				contentRating: series.contentrating,
-				firstAired   : series.firstaired,
-				genre        : series.genre.split('|').filter(Boolean),
-				imdb_id      : series.imdb_id,
-				language     : series.language,
-				network      : series.network,
-				overview     : series.overview,
-				runtime      : series.runtime,
-				status       : series.status,
-				banner       : series.banner,
-				poster       : series.poster,
-				actors       : [],
-				seasons      : []
-			});
-			_.each(episodes, function(episode) {
-				var episodeObj = {
-					episodeID    : episode.id,
-					episodeNumber: episode.episodenumber,
-					name         : episode.episodename,
-					director     : episode.director.split('|').filter(Boolean),
-					firstAired   : episode.firstaired,
-					guestStars   : episode.gueststars.split('|').filter(Boolean),
-					imdb_id      : episode.imdb_id,
-					overview     : episode.overview
-				};
-
-				var seasonIndex = _.findIndex(show.seasons, function(season) {
-					return season.seasonNumber == episode.seasonnumber;
-				});
-				if(seasonIndex > -1) {
-					show.seasons[seasonIndex].episodes.push(episodeObj);
-				}
-				else {
-					show.seasons.push({
-						seasonNumber: episode.seasonnumber,
-						seasonID    : episode.seasonid,
-						episodes    : [episodeObj]
-					});
-				}
-			});
-
-			callback(null, show);
-		},
-		function(show, callback) {
-			request.get('http://thetvdb.com/api/' + apiKey + '/series/' + seriesID + '/actors.xml', function(error, response, body) {
-				if(error)
-					next(error);
-				xmlParser.parseString(body, function(err, result) {
-					_.each(result.actors.actor, function(actor) {
-						show.actors.push({
-							name     : actor.name,
-							role     : actor.role,
-							image    : actor.image,
-							sortOrder: actor.sortorder
-						});
-					});
-
-					callback(err, show);
-				});
-			});
-		},
-		function(show, callback) {
-			show.save(function(err) {
-				if(err)
-					return next(err);
-				callback(null, "Saved successfully");
-			});
-		},
-		function(message, callback) {
-			console.log(message);
-			Show.findById(seriesID, function(err, showData) {
-				if(err)
-					return next(err);
-				callback(null, showData);
 			});
 		}
-	], endShowQuery);
+	});
 });
 
 // Default route
