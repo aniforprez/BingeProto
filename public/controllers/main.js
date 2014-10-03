96d309587856ced9e0563c08e996464a1e37edbd
Anirudh <ani.for.prez@gmail.com>
2014-10-03 23:09:55 +0530

    added typeahead to search bar

--- public/controllers/main.js
+++ public/controllers/main.js
@@ -1,8 +1,15 @@
 var BingeApp = angular.module('BingeApp');
 
-BingeApp.controller('MainCtrl', ['$scope', '$location', function($scope, $location){
+BingeApp.controller('MainCtrl', ['$scope', '$location', '$timeout', 'Search', function($scope, $location, $timeout, Search){
+	var searchDelay;
 	$scope.getSearchResults = function() {
 		if($scope.searchString)
-			$location.search('name', $scope.searchString).path('/search');
+			if(searchDelay)
+				$timeout.cancel(searchDelay);
+			searchDelay = $timeout(function() {
+				$scope.searchResults = Search.query({ searchString: $scope.searchString });
+			}, 500);
+
+			// $location.search('name', $scope.searchString).path('/search');
 	};
 }]);
