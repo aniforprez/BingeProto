bb479227564b1165bf5200e51e66df9afde7c8b6
Anirudh <ani.for.prez@gmail.com>
2014-10-04 23:06:19 +0530

    sets .searching to true when result exists and false otherwise

--- public/controllers/main.js
+++ public/controllers/main.js
@@ -3,13 +3,26 @@ var BingeApp = angular.module('BingeApp');
 BingeApp.controller('MainCtrl', ['$scope', '$location', '$timeout', 'Search', function($scope, $location, $timeout, Search){
 	var searchDelay;
 	$scope.getSearchResults = function() {
-		if($scope.searchString)
+		if($scope.searchString) {
+			$scope.searchResults = [];
 			if(searchDelay)
 				$timeout.cancel(searchDelay);
 			searchDelay = $timeout(function() {
-				$scope.searchResults = Search.query({ searchString: $scope.searchString });
+				$scope.searchResults = Search.query({ searchString: $scope.searchString }, function() {
+					if($scope.searchString) {
+						$scope.searching = true;
+					}
+				});
 			}, 500);
-
+		}
+		else {
+			if(searchDelay)
+				$timeout.cancel(searchDelay);
+			$scope.searching = false;
+		}
 			// $location.search('name', $scope.searchString).path('/search');
+	};
+	$scope.goTo = function(link) {
+		$location.path(link);
 	};
 }]);
